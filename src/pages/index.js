3cc3c1ca03987b69973bf59c7dcb47c21393855a
Bartosz Lis <lisembart97@gmail.com>
2025-06-20 21:46:44 +0200

    add more projects

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ export default function IndexPage() {
         {/* <ArticlesSection sectionId="articles" heading="Latest Articles" sources={['Medium']} /> */}
         <AboutSection sectionId="about" heading="About Me" />
         <InterestsSection sectionId="details" heading="Skills" />
-        <ProjectsSection sectionId="features" heading="My Projects" />
+        <ProjectsSection sectionId="features" heading="Portfolio" />
         <ContactSection sectionId="github" heading="Contact" />
       </Page>
     </>
